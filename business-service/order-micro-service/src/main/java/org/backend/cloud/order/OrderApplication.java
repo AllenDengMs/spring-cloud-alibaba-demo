301b2a1e71626f5f908b97d59ec78b0498ce87b2
allen <allen@mosainet.com>
2022-08-27 11:52:23 +0800

    集成openFegin

--- business-service/order-micro-service/src/main/java/org/backend/cloud/order/OrderApplication.java
+++ business-service/order-micro-service/src/main/java/org/backend/cloud/order/OrderApplication.java
@@ -3,24 +3,18 @@ package org.backend.cloud.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OrderApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(OrderApplication.class, args);
-  }
-
-  @Bean
-  @LoadBalanced
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
   }
 
   /**
--- business-service/order-micro-service/src/main/java/org/backend/cloud/order/controller/OrderController.java
+++ business-service/order-micro-service/src/main/java/org/backend/cloud/order/controller/OrderController.java
@@ -1,22 +1,20 @@
 package org.backend.cloud.order.controller;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import org.backend.cloud.order.client.UserServiceClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
 
   @Autowired
-  private RestTemplate restTemplate;
+  private UserServiceClient userServiceClient;
 
   @GetMapping("/{orderId}")
   public Map<String, Object> getUser(@PathVariable String orderId) {
@@ -25,11 +23,8 @@ public class OrderController {
     response.put("itemName", "Iphone 14");
     response.put("itemPrice", "$1500");
 
-//    String url = "http://localhost:8099/users/1";
-    String url = "http://user-service/users/1";
-    Map user = restTemplate.getForEntity(url, Map.class).getBody();
+    Map<String, Object> user = userServiceClient.getUser("1");
     response.put("buyer", user);
-    response.put("url", url);
     return response;
   }
 
